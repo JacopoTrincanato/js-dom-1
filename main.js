80139ec1776116425906125a9652df84d97409ff
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-08 15:16:49 +0200

    create the class d-none in style.css and add it to on-lightbulb via javascript

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ const lampButton = document.getElementById("button");
 const whiteLamp = document.querySelector(".off-lightbulb");
 
 const yellowLamp = document.querySelector(".on-lightbulb");
+
+//aggiungo la classe d-none all'elemento yellowLamp
+yellowLamp.classList.add("d-none");
+console.log(yellowLamp.classList);
