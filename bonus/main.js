f5efdbe8df7d8c3b4be0e1eb42d73ac2f0614a4f
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-08 16:40:15 +0200

    try to create a loop for on and off function

--- bonus/main.js
+++ bonus/main.js
@@ -11,6 +11,26 @@ console.log(yellowLamp.classList);
 
 //3. creo una funzione che mi permetta di visualizzare la lampadina accesa quando clicco il bottone
 lampButton.addEventListener("click", ()=>{
+    
     yellowLamp.classList.add("d-block");
     whiteLamp.classList.add("d-none");
 })
+
+//4. creo una funzione che mi permetta di ripetere l'azione di accensione e spegnimento
+function onAndOff(string) {
+    const myImg = document.images
+    for (let i = 0; i < myImg.length; i++) {
+        const element = myImg[i];
+        if(myImg === yellowLamp){
+            lampButton.addEventListener("click", ()=>{
+                replaceButtonText('Accendi', 'Spegni')
+            })
+        }
+    }
+    return string
+}
+
+console.log(onAndOff(lampButton));
+
+
+//cambiare il testo del bottone al click
