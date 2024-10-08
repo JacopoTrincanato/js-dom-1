b1f91dbb234e6645d0dd64d1a2da7a82bdd4b0d9
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-08 15:10:38 +0200

    add img to html and select them in javascript

---
+++ main.js
@@ -0,0 +1,6 @@
+//1. seleziono gli elementi della dom
+const lampButton = document.getElementById("button");
+
+const whiteLamp = document.querySelector(".off-lightbulb");
+
+const yellowLamp = document.querySelector(".on-lightbulb");
