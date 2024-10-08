9abdbf2e045079fec64c18ae6469c1b6f50c1d06
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-08 15:26:05 +0200

    create the function with javascript

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const whiteLamp = document.querySelector(".off-lightbulb");
 
 const yellowLamp = document.querySelector(".on-lightbulb");
 
-//aggiungo la classe d-none all'elemento yellowLamp
+//2. aggiungo la classe d-none all'elemento yellowLamp
 yellowLamp.classList.add("d-none");
 console.log(yellowLamp.classList);
+
+//3. creo una funzione che mi permetta di visualizzare la lampadina accesa quando clicco il bottone
+lampButton.addEventListener("click", ()=>{
+    yellowLamp.classList.add("d-block");
+    whiteLamp.classList.add("d-none");
+})
